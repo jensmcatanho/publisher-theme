07e8a414ee80ae1d888276b79364e465a6da353c
Matheus E. Muller <hello@memuller.com>
2017-07-26 01:54:50 -0300

    Changes 'read more' link text

--- template-parts/content.php
+++ template-parts/content.php
@@ -14,10 +14,9 @@
 	<div class="entry-content">
 		<?php
 			/* translators: %s: Name of current post */
-			the_content( sprintf(
-				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'independent-publisher-2' ), array( 'span' => array( 'class' => array() ) ) ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			) );
+			the_content(
+				__( 'Continue reading...', 'independent-publisher-2' )
+			);
 		?>
 	</div><!-- .entry-content -->
 
